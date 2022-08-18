b9c68b40738de3ceaf0a6f5fe746994e13226c4f
mathtimes <mathtimes@foxmail.com>
2022-08-18 16:22:18 +0800

    refactor SmallPrograms/Joseph Ring problem/array/main.c
    Signed-off-by: mathtimes <mathtimes@foxmail.com>

--- SmallPrograms/Joseph Ring problem/array/main.c
+++ SmallPrograms/Joseph Ring problem/array/main.c
@@ -1,50 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-int total,*people,step,p=0;
-int length()
+int length(int *people, int total)
 {
     int ret=0;
-    for(int i=0;i<total;i++)
+    for(int i=0; i<total; i++)
         if(people[i]==0)
             ret++;
     return ret;
 }
-void go(int s)
+void go(int *people, int total, int step)
 {
-    int ss=s;//还需要走的步数
-    while(ss>0)
-    {
+    static int p = 0;
+    while(step>0) {
         if(p>=total)
             p-=total;
-        if(people[p]==0)
-        {
+        if(people[p]==0) {
             p++;
-            ss--;
-        }
-        else if(people[p]==1)
-        {
+            step--;
+        } else if(people[p]==1)
             p++;
-        }
     }
-    while(people[p]==1)
-    {
-        p++;
-        if(p>=total)
-        p-=total;
-    }
-    people[p]=1;
-    for(int i=0;i<total;i++)
-    {    
+    p--;
+    people[p] = 1;
+    for(int i=0; i<total; i++)
         printf("\e[%dm%d ",32-people[i],people[i]);
-    }
-    printf("\e[33m\t%d\n", length());
+    printf("\e[33m\t%d\n", length(people, total));
 }
 int main(int argc,char **argv)
 {
-    total=atoi(argv[1]);
-    people=calloc(total,sizeof(int));
-    step=atoi(argv[2]);
-    while(length()>1)
-        go(step);
+    if(argc!=3)
+        return 0;
+    int total=atoi(argv[1]);
+    int *people=(int *)calloc(total,sizeof(int));
+    int step=atoi(argv[2]);
+    while(length(people, total)>1)
+        go(people, total, step);
+    int i;
+    for(i=0;people[i];i++)
+        printf("  ");
+    printf("%d\n\e[0m", i+1);
+    
     return 0;
 }
