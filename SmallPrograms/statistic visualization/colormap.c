ab01addbd3df32d4bbf915c73c4e4d68c4253da3
mathtimes <mathtimes@foxmail.com>
2022-08-25 22:48:14 +0800

    solve some bugs: x-y traverse position Blue channel line function (I assumed i = j)
    Signed-off-by: mathtimes <mathtimes@foxmail.com>

--- SmallPrograms/statistic visualization/colormap.c
+++ SmallPrograms/statistic visualization/colormap.c
@@ -3,21 +3,21 @@
 #include <string.h>
 #include <math.h>
 
-#define XLEN 1024
-#define YLEN 1024
+#define XLEN 2560
+#define YLEN 1600
 
 int main()
 {
     FILE *fp = fopen("out.ppm", "wb");
     
     fprintf(fp, "P6\n%d %d\n255\n", XLEN, YLEN);
-    for(int i=0;i<XLEN;i++)
-        for(int j=0;j<YLEN;j++)
-            fprintf(fp, "%c%c%c\0", 
-                i*256/XLEN, 
-                j*256/YLEN, 
-                255-((i-j)*(i-j)*256/XLEN/YLEN)
-            );//RGB
+    for(int i=0;i<YLEN;i++)
+        for(int j=0;j<XLEN;j++)
+            fprintf(fp, "%c%c%c", 
+                i*256/YLEN, 
+                j*256/XLEN, 
+                255-((i-YLEN*j/XLEN)*(i-YLEN*j/XLEN)*256/XLEN/YLEN)
+            );
     
     fclose(fp);
     return 0;
