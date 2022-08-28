d491e424720bf72a5cb15997cb5b97d5df836066
mathtimes <mathtimes@foxmail.com>
2022-08-28 22:04:54 +0800

    modified color map
    Signed-off-by: mathtimes <mathtimes@foxmail.com>

--- StatisticVisualization/Julia_sdl.c
+++ StatisticVisualization/Julia_sdl.c
@@ -1,16 +1,17 @@
-#include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
-//窗口大小
-#define XDIM 512
-#define YDIM 512
-
-#define NUM 512   //迭代次数
+#include <time.h>
+#include <sdl2/sdl.h>
 
 #define PI 3.141592653589793
+
+#define XDIM 256
+#define YDIM 256
+
+#define NUM 512    //迭代次数
+
 #define RMNEG(_X_) ((_X_)>=0?(_X_):0)
 
 struct {
@@ -20,18 +21,21 @@ struct {
     double y_range;
 } args;
 
-struct COMPLEX {
+struct COMPLEX
+{
     double re;
     double im;
 };
+
+struct COMPLEX c;
+
 int tof(int i, int j)
 {
-    struct COMPLEX z = {0,0};
-    struct COMPLEX c = {
+    struct COMPLEX z = {
         args.x_offset+(double)i/XDIM*args.x_range, 
         args.y_offset+(1-(double)j/YDIM)*args.y_range
     };
-    for(int t=1; t<=NUM; t++) {
+    for(int t=1;t<=NUM;t++){
         double z_re_bak = z.re;
         z.re = z.re*z.re-z.im*z.im+c.re;
         z.im = 2*z_re_bak*z.im+c.im;
@@ -41,7 +45,8 @@ int tof(int i, int j)
     return 0;
 }
 
-struct RGB {
+struct RGB
+{
     unsigned char R;
     unsigned char G;
     unsigned char B;
@@ -51,9 +56,9 @@ struct RGB translate2RGB(int code)
     struct RGB ret = {0, 0, 0}; //返回局部变量，危险
     if(code){
         double log_code = log(code)/log(NUM);   //from 0 to 1
-        ret.R = (int)(RMNEG(-cos(log_code*PI))*256);
+        ret.R = (int)(RMNEG(cos(log_code*PI))*256);
         ret.G = (int)(RMNEG(sin(log_code*PI))*256);
-        ret.B = (int)(RMNEG(cos(log_code*PI))*256);
+        ret.B = (int)(RMNEG(-cos(log_code*PI))*256);
     }
     return ret;
 }
@@ -77,18 +82,28 @@ void draw(SDL_Renderer *renderer)
 
 int main(int argc, char *argv[])
 {
-    if(argc!=5){
-        args.x_offset = 0;
-        args.y_offset = 0;
-        args.x_range  = 1;
-        args.y_range  = 1;
-    } else {
+    if(argc==5){
         args.x_offset = atof(argv[1]);
         args.y_offset = atof(argv[2]);
         args.x_range  = atof(argv[3]);
         args.y_range  = atof(argv[4]);
+    }else {
+        args.x_offset = 0;
+        args.y_offset = 0;
+        args.x_range  = 1;
+        args.y_range  = 1;
     }
-    
+    if(argc==3) {
+        c.re = atof(argv[1]);
+        c.im = atof(argv[2]);
+    }else {
+        srand(time(NULL));
+        c.re = (double)rand()/RAND_MAX;
+        c.im = (double)rand()/RAND_MAX;
+    }
+    printf("args.x_offset = %f\nargs.y_offset = %f\n", args.x_offset, args.y_offset);
+    printf("args.x_range = %f\nargs.y_range = %f\n", args.x_range, args.y_range);
+    printf("c = %f + %f I\n", c.re, c.im);
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *Window = SDL_CreateWindow("Draw Mandelbrot Set With SDL2",
                                           SDL_WINDOWPOS_UNDEFINED,
